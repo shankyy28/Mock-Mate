f7d6f32ff5c7a7df3d3e1c8238ccb22ec90e629b
Rinku Kharbanda <rinkukharbanda@Candys-mac.local>
2024-09-15 08:53:19 +0530

    fix: App.js

--- login-signup/src/App.js
+++ login-signup/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginSignup from './scenes/LoginSignup/LoginSignup';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { darkTheme } from './theme'; // Import the dark theme
+import { darkTheme } from './theme'; 
+import Dashboard from './scenes/Dashboard/Dashboard';
 import Topbar from './scenes/global/Topbar';
 import { Sidebar } from 'react-pro-sidebar';
 
 function App() {
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
@@ -16,11 +18,11 @@ function App() {
           <Routes>
             <Route path="/" element={<LoginSignup />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/profile" element={<Profile />} />
+            {/* <Route path="/profile" element={<Profile />} />
             <Route path="/study-material" element={<Studymaterial />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/scores" element={<Score />} />
-            <Route path="/aboutus" element={<Aboutus />} />
+            <Route path="/aboutus" element={<Aboutus />} /> */}
           </Routes>
         </main>
       </div>
